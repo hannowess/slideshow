e2f90e9159e46fcbf382609e37187cd1015e6eba
hannowess <hannowessels@gmail.com>
2015-02-14 14:42:06 +0200

    lesson 14feb

---
+++ js/testrun.js
@@ -0,0 +1,13 @@
+//var test=68/9;
+//console.debug(test);
+//function adding (x,y){
+//	var result=x+y;
+//	return result;
+//}
+//alert(adding(2658,1453));
+
+function multiply (x){
+	var result=x*10;
+	return result;
+}
+console.debug(multiply(98));
